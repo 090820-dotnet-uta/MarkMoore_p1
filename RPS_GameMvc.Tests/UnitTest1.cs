004b86f8f6a72e0a24056a82a0d5515bf36188cb
Mark Moore <mark.moore@revature.com>
2020-10-02 14:11:53 -0500

    test push

--- RPS_GameMvc.Tests/UnitTest1.cs
+++ RPS_GameMvc.Tests/UnitTest1.cs
@@ -1,14 +1,38 @@
 using System;
 using Xunit;
+using RPS_GameMvc.Models;
+using RPS_GameMvc.GamePlay;
 
 namespace RPS_GameMvc.Tests
 {
 	public class UnitTest1
 	{
+
+		
 		[Fact]
 		public void Test1()
 		{
 			Assert.Equal(1,1);
+		}
+
+		[Fact]
+		public void GameAddPlayerReturnsThePlayer()
+		{
+			//Arrange
+			Rps_Game rpsgame = new Rps_Game();
+			Player p = new Player()
+			{
+				Name = "jim jim",
+				Losses = 2,
+				Wins = 4
+			};
+
+			//act
+			Player q = rpsgame.GameAddPlayer(p);
+
+			//assert
+			Assert.Equal("jim jim", q.Name);
+
 		}
 	}
 }
--- RpsGame/GamePlay/Rps_Game.cs
+++ RpsGame/GamePlay/Rps_Game.cs
@@ -18,6 +18,8 @@ namespace RPS_GameMvc.GamePlay
 		private IMemoryCache _cache;
 		private readonly DbContextClass _context;
 
+		public Rps_Game(){}
+
 		public Rps_Game(ILogger<Rps_Game> logger, IMemoryCache cache, DbContextClass context)
 		{
 			_logger = logger;
